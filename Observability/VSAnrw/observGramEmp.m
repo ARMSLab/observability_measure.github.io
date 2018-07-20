f98a466b3d297350d1627c71ccf95c5ee51573be
bexultanrakhim <bexultan.rakhim@nu.edu.kz>
2018-07-20 15:10:24 +0600

    Delete observGramEmp.m

--- Observability/VSAnrw/observGramEmp.m
+++
@@ -1,8 +0,0 @@
-function W_emp = observGramEmp(C,t,Ts,x,epsi,u,model)
-    W_emp = zeros(size(x,1));
-    for ind=1:length(x)
-        
-        W= empGram(ind,C,x,u,epsi,Ts,t,model);
-        W_emp = W_emp + W;
-    end
-end
