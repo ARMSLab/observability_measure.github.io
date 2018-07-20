06fe3373ddbe3032fbd5d2963117529a43ff6895
bexultanrakhim <bexultan.rakhim@nu.edu.kz>
2018-07-20 15:21:59 +0600

    Delete simple_constraints.m

--- Observability/VSAwrw/simple_constraints.m
+++
@@ -1,8 +0,0 @@
-function [Acon,Bcon] = simple_constraints(umax,umin,np,nu)
-    Ac = [1;-1];
-    Acon = zeros(2*nu*np,nu*np);
-    for ind1=1:np
-        Acon((2*(ind1-1)+1):2*ind1,(1*(ind1-1)+1):ind1)=Ac;
-    end
-    Bcon = repmat([umax; -umin],np,1);
-end
